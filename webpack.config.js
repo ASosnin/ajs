0d76b42753e21a164d24d5bfc05cf21e89f6890e
Andrey.Sosnin <woodogul@yandex.ru>
2021-10-17 12:53:48 +0300

    Библиотека, файлы сборки.

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'ajs',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+    globalObject: 'this',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  }
+}
